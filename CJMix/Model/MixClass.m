47d3884bea522a10f8f6b08bc298085d314a3ee2
Chan <467116811@qq.com>
2019-01-24 18:33:12 +0800

    添加方法名获取，并不完善

--- CJMix/Model/MixClass.m
+++ CJMix/Model/MixClass.m
@@ -57,7 +57,6 @@
                     }
                 }];
                 
-                
                 self.method.addMethod = [NSArray arrayWithArray:addMethods];
                 self.method.subMethod = [NSArray arrayWithArray:subMethods];
                 
--- CJMix/Strategy/MixMethodStrategy.m
+++ CJMix/Strategy/MixMethodStrategy.m
@@ -7,13 +7,42 @@
 //
 
 #import "MixMethodStrategy.h"
+#import "MixStringStrategy.h"
 
 @implementation MixMethodStrategy
 
 + (NSString *)methodFromData:(NSString *)data {
-    
-    
-    
+    NSString * methodData = [data copy];
+    if ([methodData containsString:@"\n"] && [methodData containsString:@")"] && [methodData containsString:@";"]) {
+        NSArray <NSString *>* segmentation = [methodData componentsSeparatedByString:@")"];
+        NSString * method = nil;
+        if (segmentation.count > 1) {
+            method = segmentation[1];
+        }
+        if (method) {
+            if ([method containsString:@";"]) {
+                //可能是方法
+                if ([method containsString:@":"]) {
+                    //带参数方法
+                    NSArray <NSString *>* segmentation = [method componentsSeparatedByString:@":"];
+                    if (segmentation.count) {
+                        method = [NSString stringWithFormat:@"%@:",segmentation[0]];
+                    }
+                    
+                } else {
+                    //不带参数方法
+                    NSArray <NSString *>* segmentation = [method componentsSeparatedByString:@";"];
+                    if (segmentation.count) {
+                        method = segmentation[0];
+                    }
+                }
+            }
+        }
+        
+        if ([MixStringStrategy isAlphaNum:method]) {
+            return method;
+        }
+    }
     
     return nil;
 }
