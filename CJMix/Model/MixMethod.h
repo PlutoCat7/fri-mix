36c29c09078bc550df82dab0fb650bb8e9ddaecd
Chan <467116811@qq.com>
2019-01-24 17:43:41 +0800

    移除无用注释

--- CJMix/Model/MixMethod.h
+++ CJMix/Model/MixMethod.h
@@ -8,8 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-
-
 @interface MixMethod : NSObject
 
 @property (nonatomic , copy) NSArray <NSString *>* addMethod;
--- CJMix/Model/MixObject.h
+++ CJMix/Model/MixObject.h
@@ -13,19 +13,11 @@
 
 @interface MixObject : NSObject
 
-
 @property (nonatomic , strong , readonly) MixClassFile* classFile;
 
-/**
- 一般来说.h可以全局替换
- */
 @property (nonatomic , strong , readonly) NSArray <MixClass *> *hClasses;
 
-/**
- 一般来说.m只可以局部替换
- */
 @property (nonatomic , strong , readonly) NSArray <MixClass *> *mClasses;
-
 
 - (instancetype)initWithClassFile:(MixClassFile *)file;
 
