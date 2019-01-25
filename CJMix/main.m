0ef0eedb73e5bd44680e22874025eb77701a3f95
Chan <467116811@qq.com>
2019-01-25 18:44:29 +0800

    调整命名

--- CJMix/main.m
+++ CJMix/main.m
@@ -22,15 +22,15 @@ int main(int argc, const char * argv[]) {
 //        NSString * referencePath = @"/Users/wangsw/Desktop/Reference";
 //        NSString * rootPath = @"/Users/wangsw/Desktop/najiabao-file";
         
-        [MixConfig sharedSingleton].shieldPaths = @[@"imkit",@"imsdk",@"FDFullscreenPopGesture",@"UIAlertView+BlocksKit",@"V8HorizontalPickerView",@"MJExtension",@"FBKVOController",@"countly-sdk-ios",@"Hockey-iOS",@"ElvaChatServiceSDK"];
+        [MixConfig sharedSingleton].shieldPaths = @[@"imkit",@"imsdk",@"FDFullscreenPopGesture",@"UIAlertView+BlocksKit",@"V8HorizontalPickerView",@"MJExtension",@"FBKVOController",@"countly-sdk-ios",@"Hockey-iOS",@"ElvaChatServiceSDK",@"WebViewJavascriptBridge"];
         
         
         NSString * referencePath = @"/Users/wn/Desktop/Reference";
         NSString * rootPath = @"/Users/wn/Documents/git/WonderVoice/Trunk/AudioRoom";
-        NSString * path = [NSString stringWithFormat:@"%@_copy",rootPath];
+        NSString * copyPath = [NSString stringWithFormat:@"%@_copy",rootPath];
         
         printf("拷贝文件中..\n");
-        BOOL isSuccess = [MixFileStrategy copyItemAtPath:rootPath toPath:path overwrite:YES error:nil];
+        BOOL isSuccess = [MixFileStrategy copyItemAtPath:rootPath toPath:copyPath overwrite:YES error:nil];
         if (!isSuccess) {
             printf("拷贝文件失败\n");
             return 0;
@@ -39,7 +39,7 @@ int main(int argc, const char * argv[]) {
         printf("获取替换类名\n");
         NSArray <NSString *>* className = [MixReferenceStrategy classNamesWithPath:referencePath];
         printf("获取可替换类\n");
-        NSArray <MixObject*>* objects = [MixObjectStrategy objectsWithPath:path];
+        NSArray <MixObject*>* objects = [MixObjectStrategy objectsWithPath:copyPath];
         printf("开始替换类名\n");
         [MixMainStrategy replaceClassName:objects referenceClassNames:className];
         printf("结束替换类名\n");
@@ -47,13 +47,13 @@ int main(int argc, const char * argv[]) {
         
         
         
-        MixFile * file = [MixFileStrategy projectWithFilesWithPath:path];
-        if (file) {
-            
-        } else {
-            printf("工程文件不存在\n");
-        }
-        //
+//        MixFile * file = [MixFileStrategy projectWithFilesWithPath:path];
+//        if (file) {
+//
+//        } else {
+//            printf("工程文件不存在\n");
+//        }
+        
         printf("开始替换文件名称\n");
         if ([MixFileNameStrategy start:objects rootPath:rootPath]) {
             printf("成功替换文件名称\n");
