c67aabf6db5d8a8e940cc3e6c1b72477817616e4
Chan <467116811@qq.com>
2019-01-30 00:09:30 +0800

    新增：地址调整

--- CJMix/main.m
+++ CJMix/main.m
@@ -30,18 +30,17 @@ int main(int argc, const char * argv[]) {
 
 
 #if 1
-        NSString * referencePath = @"/Users/wangsw/wangle/majiabao/Reference";
+        //NSString * referencePath = @"/Users/wangsw/wangle/majiabao/Reference";
+        //NSString * rootPath = @"/Users/wangsw/wangle/majiabao/AudioRoom";
         //NSString * rootPath = @"/Users/wangsw/wangle/majiabao/najiabao-file";
-        NSString * rootPath = @"/Users/wangsw/wangle/majiabao/AudioRoom";
+        NSString * referencePath = @"/Users/wn/Desktop/Reference";
+        NSString * rootPath = @"/Users/wn/Documents/git/WonderVoice/Trunk/AudioRoom";
 #else
         NSString * referencePath = @"/Users/yegaofei/Desktop/ygf_project/Rongle/wangle_src/Reference";
         NSString * rootPath = @"/Users/yegaofei/Desktop/ygf_project/Rongle/wangle_src/WonderVoice/Trunk/AudioRoom";
 #endif
         
         NSString * copyPath = [NSString stringWithFormat:@"%@_mix",rootPath];
-        
-        NSString * sdkPath = @"/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform";
-
 
         printf("拷贝文件中..\n");
         BOOL isSuccess = [MixFileStrategy copyItemAtPath:rootPath toPath:copyPath overwrite:YES error:nil];
@@ -55,22 +54,19 @@ int main(int argc, const char * argv[]) {
         NSArray <MixObject*>* referenceObjects = [MixObjectStrategy objectsForKey:@"mix_reference"];
         if (!referenceObjects) {
             referenceObjects = [MixObjectStrategy objectsWithPath:referencePath];
+            [MixObjectStrategy saveObjects:referenceObjects key:@"mix_reference"];
         }
-        [MixObjectStrategy saveObjects:referenceObjects key:@"mix_reference"];
         
         printf("获取需要被替换对象\n");
         NSArray <MixObject*>* copyObjects = [MixObjectStrategy objectsWithPath:copyPath saveConfig:YES];
-        
         
         printf("获取替换类名\n");
         NSArray <NSString *>* classNames = [MixReferenceStrategy classNamesWithObjects:referenceObjects];
         printf("开始替换类名\n");
         [MixMainStrategy replaceClassName:copyObjects referenceClassNames:classNames];
         printf("结束替换类名\n");
-        
-        
-    
-        printf("获取系统对象\n");
+
+        printf("获取系统方法\n");
         NSArray <NSString *> * systemMethods = [MixMethodStrategy systemMethods];
         NSString * podPath = @"/Users/wn/Documents/git/WonderVoice/Trunk/AudioRoom/Pods";
         NSArray <NSString *> * podsMethods = [MixMethodStrategy methodsWithPath:podPath];
